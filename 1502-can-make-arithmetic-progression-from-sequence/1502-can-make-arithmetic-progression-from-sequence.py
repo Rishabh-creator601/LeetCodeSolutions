01d2489d47917291c8cb2014444895f4df8ebe89
Rishabh <rashesh369@gmail.com>
2026-07-09 21:21:22 +0530

    Time: 3 ms (21.47%), Space: 19.1 MB (95.5%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -7,9 +7,7 @@ class Solution:
         d =  abs(arr[1] - arr[0])
 
         for i in range(0,len(arr)-1):
-
             if abs(arr[i+1] - arr[i]) != d :
-                print(arr[i+1] - arr[i])
                 flag = False 
 
         return flag 
